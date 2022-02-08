a4a755e1cd8b9e1fa02e2bfc212fc479381bad4d
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-02-08 18:46:10 +0900

    feat: smaller

--- scripts/create.sh
+++ scripts/create.sh
@@ -2,6 +2,6 @@
 
 tmux split-window -v -c "#{pane_current_path}"
 tmux split-window -h -c "#{pane_current_path}"
-tmux resize-pane -D 15
+tmux resize-pane -D 10
 tmux select-pane -t 0
 tmux send-keys "vim ." Enter
